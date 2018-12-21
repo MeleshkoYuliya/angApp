64d25a6f791ef0c1c3cf631bf8cbd5e714974e2e
JuLiAmEleshk <yulia_lang@mail.ru>
2018-12-21 17:40:15 +0300

    addd user

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,14 +11,45 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+
+  submited = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //set whole form
+    // this.singupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    //set not whole form
+    this.singupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
   onSubmit() {
-    console.log(this.singupForm);
+    this.submited = true;
+    this.user.username = this.singupForm.value.userData.username;
+    this.user.email = this.singupForm.value.userData.email;
+    this.user.secretQuestion = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
   }
 }
